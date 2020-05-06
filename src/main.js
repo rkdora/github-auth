0c55a9b21e658cd61f05465610645da4fc260262
ryuto <rryuto5210@gmail.com>
2020-05-06 12:46:02 +0900

    Add Github Auth

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
+import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "github-auth-6d6da.firebaseapp.com",
+  databaseURL: "https://github-auth-6d6da.firebaseio.com",
+  projectId: "github-auth-6d6da",
+  storageBucket: "github-auth-6d6da.appspot.com",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
